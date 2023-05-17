42eca93f52119f71ad02e0676a2124e03c731dc1
Davi Batista <91736880+daviebatista@users.noreply.github.com>
2023-05-17 09:07:04 -0300

    Rolling cap fix

--- commands/Dices.js
+++ commands/Dices.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js')
 
-
+//Importação do modulo discord.js
 module.exports = {
   data: new SlashCommandBuilder()
       .setName('rl')
@@ -26,21 +26,191 @@ const resultsArr = []
 //-------
 
 function exaltedD10(){
-  const d10 = Math.floor(Math.random()*10 + 1)
-  if(d10 == 10){
-    const print = 2
-    resultsArr.push(` **${d10}**`)
-    succesesArr.push(print)
-  }else if(d10 >= 7){
-    const print = 1
-    resultsArr.push(` ${d10}`)
-    succesesArr.push(print)
-  }else{
-    const print = 0
-    resultsArr.push(` ~~${d10}~~`)
-    succesesArr.push(print)
+  const firstNumber = Math.floor(Math.random()*3 + 1)
+  //Rolagem fraca
+  if  (firstNumber == 1)  {
+      const selector = Math.floor(Math.random()*4 + 1)
+    if  (selector == 1 || 2) {
+      const d10 = Math.floor(Math.random()*8 + 2)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 3) {
+      const d10 = Math.floor(Math.random()*10 + 1)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 4) {
+      const d10 = Math.floor(Math.random()*10 + 1)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
   }
-  return d10 
+  //Rolagem mediana
+  if  (firstNumber == 2)  {
+    const selector = Math.floor(Math.random()*4 + 1)
+    if  (selector == 1) {
+      const d10 = Math.floor(Math.random()*8 + 2)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 2 || 3) {
+      const d10 = Math.floor(Math.random()*10 + 1)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 4) {
+      const d10 = Math.floor(Math.random()*9 + 2)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+  }
+  //Rolagem boa
+  if  (firstNumber == 3)  {
+    const selector = Math.floor(Math.random()*4 + 1)
+    if  (selector == 1) {
+      const d10 = Math.floor(Math.random()*7 + 3)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 2) {
+      const d10 = Math.floor(Math.random()*7 + 4)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+    if  (selector == 3 || 4) {
+      const d10 = Math.floor(Math.random()*6 + 5)
+      if(d10 == 10){
+        const print = 2
+        resultsArr.push(` **${d10}**`)
+        succesesArr.push(print)
+      }else if(d10 >= 7){
+        const print = 1
+        resultsArr.push(` ${d10}`)
+        succesesArr.push(print)
+      }else{
+        const print = 0
+        resultsArr.push(` ~~${d10}~~`)
+        succesesArr.push(print)
+      }
+      console.log(`Resultado: ${selector}`)
+      console.log(`Dado: ${d10}`)
+      return d10 
+    }
+  }
+  
   
 }
 
@@ -57,3 +227,5 @@ const sum = succesesArr.reduce(
   return sum
 }
 //---------------------------------------
+
+exalted(100)
